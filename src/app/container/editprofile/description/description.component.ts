09322ccfde99b82ffd603592201b9781e3dc105c
yuriyoon1009 <yuriyoon1009@gmail.com>
2017-11-29 10:32:48 +0900

    form 마크업

--- src/app/container/editprofile/description/description.component.ts
+++ src/app/container/editprofile/description/description.component.ts
@@ -5,11 +5,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './description.component.html',
   styleUrls: ['./description.component.css']
 })
-export class DescriptionComponent implements OnInit {
+export class DescriptionComponent {
 
-  constructor() { }
+  // 비로그인
+  lists: any[] = [
+    {title: '호스팅하기', cols: 1},
+    {title: '이벤트', cols: 1},
+    {title: '회원가입', cols: 1},
+    {title: '로그인', cols: 1}
+  ];
 
-  ngOnInit() {
+  restColspan() {
+    const listColspan = this.lists.map(list => list.cols);
+    let listColspanTotal = 0;
+    listColspan.forEach(list => listColspanTotal += list);
+    // logo colspan 2 빼면 전체 cols 10
+    return 11 - listColspanTotal;
   }
+
 
 }
--- src/app/container/editprofile/editprofile.component.ts
+++ src/app/container/editprofile/editprofile.component.ts
@@ -1,52 +1,98 @@
-import { Component, OnInit } from '@angular/core';
+import { FormsModule, FormGroup, FormBuilder, Validators, NgModel } from '@angular/forms';
+import { Component, OnInit, NgModule } from '@angular/core';
+
 
 @Component({
   selector: 'app-editprofile',
-  templateUrl: './editprofile.component.html',
+  template: `
+  <mat-card>
+    <!--lastname-->
+    <mat-form-field class="block">
+      <!--placeholder는 사용자 정보 -->
+      <input matInput type="text" name="firstName" formControlName="firstName" placeholder="이름"/>
+    </mat-form-field>
+    <!--lastname-->
+    <!--firstname-->
+    <mat-form-field class="block">
+      <input matInput type="text" name="lastName" formControlName="lastName"
+      placeholder="성"/>
+    </mat-form-field>
+    <!--firstname-->
+    <!--sex-->
+   <!-- <mat-form-field class="block">
+      <mat-select placeholder="성별">
+        <mat-option *ngFor="let test for tests" [value]="test.value">
+          {{test.value}}
+        </mat-option>
+      </mat-select>
+    </mat-form-field>-->
+    <!--sex-->
+    <!--birthday-->
+    <mat-form-field>
+      <mat-select placeholder="일" class="date">
+        <mat-option></mat-option>
+      </mat-select>
+      <mat-select placeholder="월" class="date">
+        <mat-option></mat-option>
+      </mat-select>
+      <mat-select placeholder="년" class="date">
+        <mat-option></mat-option>
+      </mat-select>
+    </mat-form-field>
+    <mat-form-field class="block">
+      <input matInput type="email" name="email" formControlName="email" placeholder="메일"/>
+    </mat-form-field>
+    <mat-form-field class="block">
+      <input matInput type="number" name="number" formControlName="number" placeholder="전화번호"/>
+    </mat-form-field>
+    <mat-form-field class="block">
+      <mat-select placeholder="선호하는 언어">
+        <mat-option></mat-option>
+      </mat-select>
+    </mat-form-field>
+    <mat-form-field class="block">
+      <input matInput type="text" name="whereIam" formControlName="whereIam"
+      placeholder="거주도시"/>
+    </mat-form-field>
+    <mat-form-field class="block">
+      <textarea matInput name="introduction" formControlName="introduction" placeholder="자기소개"></textarea>
+    </mat-form-field>
+    <!--birthday-->
+    <!--<mat-form-field>
+      <mat-select placeholder="Colors" [(ngModel)]="selectedColor">
+        <mat-option *ngFor="let color of colors" [value]="color.value">
+        {{ color.label }}
+        </mat-option>
+      </mat-select>
+    </mat-form-field>-->
+    <button mat-raised-button type="submit">저장하기</button>
+  </mat-card>
+  `,
   styleUrls: ['./editprofile.component.css']
 })
-export class EditprofileComponent {
+export class EditprofileComponent implements OnInit {
 
-  // 비로그인
-  lists: any[] = [
-    {title: '호스팅하기', cols: 1},
-    {title: '이벤트', cols: 1},
-    {title: '회원가입', cols: 1},
-    {title: '로그인', cols: 1}
+  // editProfile 변수 FormGroup 객체 타입
+  editProfile: FormGroup;
+  selectedColor: string;
+
+  tests = [
+    {value: '여자'},
+    {value: '남자'}
   ];
-
- 
-
-  tiles: any[] = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  colors = [
+    {value: 'red', label: 'Red'},
+    {value: 'green', label: 'Green'},
+    {value: 'blue', label: 'Blue'},
+    {value: 'cyan', label: 'Cyan'},
+    {value: 'magenta', label: 'Magenta'},
+    {value: 'yellow', label: 'Yellow'},
+    {value: 'black', label: 'Black'},
   ];
-
-  dogs: Object[] = [
-    { name: 'Porter', human: 'Kara' },
-    { name: 'Mal', human: 'Jeremy' },
-    { name: 'Koby', human: 'Igor' },
-    { name: 'Razzle', human: 'Ward' },
-    { name: 'Molly', human: 'Rob' },
-    { name: 'Husi', human: 'Matias' },
-  ];
-
-  basicRowHeight = 80;
-  fixedCols = 4;
-  fixedRowHeight = 100;
-  ratioGutter = 1;
-  fitListHeight = '400px';
-  ratio = '4:1';
-
-  addTileCols() { this.tiles[2].cols++; }
-
-  restColspan() {
-    const listColspan = this.lists.map(list => list.cols);
-    let listColspanTotal = 0;
-    listColspan.forEach(list => listColspanTotal += list);
-    // logo colspan 2 빼면 전체 cols 10 
-    return 11 - listColspanTotal;
+  constructor(public fb: FormBuilder) {}
+  ngOnInit() {
+    this.editProfile = this.fb.group({
+      firstName: ['', [Validators.required]]
+    });
   }
 }
